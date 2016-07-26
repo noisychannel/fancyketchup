dbad39135febd30b0909fa80e85c77cda8a32939
Gaurav Kumar <gaurav.bison@gmail.com>
2016-07-26 16:11:59 -0400

    Structure changes to the LSTM classifier

--- examples/lstm_classifier/train.py
+++ examples/lstm_classifier/train.py
@@ -4,137 +4,28 @@ Build a tweet sentiment analyzer
 
 from __future__ import print_function
 
+import os
+import sys
 import time
 import pickle
 import numpy
 import theano
 import theano.tensor as T
-from collections import OrderedDict
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
-from cutils.numeric import numpy_floatX
 from cutils.training.utils import get_minibatches_idx, weight_decay
-from cutils.layers.utils import dropout_layer
-from cutils.layers.lstm import LSTM
-from cutils.params.utils import zipp, unzip, load_params, init_tparams
+from cutils.params.utils import zipp, unzip, load_params
 from cutils.data_interface.utils import pad_and_mask
 from cutils.training.trainer import adadelta
 
+# Include current path in the pythonpath
+script_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(script_path)
+
 import imdb
+from classifier import LSTM_CF
 
 SEED = 123
 numpy.random.seed(SEED)
-
-layers = {}
-
-
-def _p(pp, name):
-    return '%s_%s' % (pp, name)
-
-
-def init_params(dim_proj, ydim, word_dict):
-    """
-    Embedding and classifier params
-    """
-    def unpack(source, target):
-        for kk, vv in source.items():
-            target[kk] = vv
-
-    rng = numpy.random.RandomState(SEED)
-    params = OrderedDict()
-    tparams = OrderedDict()
-    # Add parameters from dictionary
-    unpack(word_dict.params, params)
-    unpack(word_dict.tparams, tparams)
-    # Initialize LSTM and add its params
-    layers['lstm'] = LSTM(dim_proj, rng)
-    unpack(layers['lstm'].params, params)
-    unpack(layers['lstm'].tparams, tparams)
-    # Initialize other params
-    other_params = OrderedDict()
-    other_params['U'] = 0.01 * numpy.random.randn(dim_proj, ydim) \
-        .astype(theano.config.floatX)
-    other_params['b'] = numpy.zeros((ydim,)).astype(theano.config.floatX)
-    other_tparams = init_tparams(other_params)
-    unpack(other_params, params)
-    unpack(other_tparams, tparams)
-
-    return params, tparams
-
-
-def build_model(tparams, options):
-    use_noise = theano.shared(numpy_floatX(0.))
-    x = T.matrix('x', dtype='int64')
-    mask = T.matrix('mask', dtype=theano.config.floatX)
-    y = T.vector('y', dtype='int64')
-
-    n_timesteps = x.shape[0]
-    n_samples = x.shape[1]
-
-    emb = tparams['Wemb'][x.flatten()].reshape([n_timesteps,
-                                                n_samples,
-                                                options['dim_proj']])
-    proj = layers['lstm'].lstm_layer(emb, options['dim_proj'], mask=mask)
-    if options['encoder'] == 'lstm':
-        #TODO: What the shit is happening here?
-        proj = (proj * mask[:, :, None]).sum(axis=0)
-        proj = proj / mask.sum(axis=0)[:, None]
-    if options['use_dropout']:
-        trng = RandomStreams(SEED)
-        proj = dropout_layer(proj, use_noise, trng)
-
-    pred = T.nnet.softmax(T.dot(proj, tparams['U']) + tparams['b'])
-
-    f_pred_prob = theano.function([x, mask], pred, name='f_pred_prob')
-    f_pred = theano.function([x, mask], pred.argmax(axis=1), name='f_pred')
-
-    off = 1e-8
-    if pred.dtype == 'float16':
-        off = 1e-6
-
-    cost = -T.log(pred[T.arange(n_samples), y] + off).mean()
-
-    return use_noise, x, mask, y, f_pred_prob, f_pred, cost
-
-
-def pred_probs(f_pred_prob, data, iterator, verbose=False):
-    """
-    Probabilities for new examples from a trained model
-    """
-    n_samples = len(data[0])
-    probs = numpy.zeros((n_samples, 2)).astype(theano.config.floatX)
-
-    n_done = 0
-
-    for _, valid_index in iterator:
-        x, mask, y = pad_and_mask([data[0][t] for t in valid_index],
-                                  numpy.array(data[1])[valid_index],
-                                  maxlen=None)
-        pred_probs = f_pred_prob(x, mask)
-        probs[valid_index, :] = pred_probs
-
-        n_done += len(valid_index)
-        if verbose:
-            print("%d/%d samples classified" % (n_done, n_samples))
-
-    return probs
-
-
-def pred_error(f_pred, data, iterator, verbose=False):
-    """
-    Errors for samples for a trained model
-    """
-    valid_err = 0
-    for _, valid_index in iterator:
-        x, mask, y = pad_and_mask([data[0][t] for t in valid_index],
-                                  numpy.array(data[1])[valid_index],
-                                  maxlen=None)
-        preds = f_pred(x, mask)
-        targets = numpy.array(data[1])[valid_index]
-        valid_err += (preds == targets).sum()
-    valid_err = 1. - numpy_floatX(valid_err) / len(data[0])
-
-    return valid_err
 
 
 def train_lstm(
@@ -178,27 +69,26 @@ def train_lstm(
 
     print('Building model')
     # Create the initial parameters for the model
-    params, tparams = init_params(model_options['dim_proj'], ydim,
-                                  imdb_data.dictionary)
+    lstm_cf = LSTM_CF(model_options['dim_proj'], ydim,
+                      imdb_data.dictionary, SEED)
 
     if reload_model:
-        load_params('lstm_model.npz', params)
+        load_params('lstm_model.npz', lstm_cf.params)
         # Update the tparams with the new values
-        zipp(params, tparams)
+        zipp(lstm_cf.params, lstm_cf.tparams)
 
     # Create the shared variables for the model
-    (use_noise, x, mask,
-     y, f_pred_prob, f_pred, cost) = build_model(tparams, model_options)
+    (use_noise, x, mask, y, cost) = lstm_cf.build_model()
 
     if decay_c > 0.:
-        cost += weight_decay(cost, tparams['U'], decay_c)
+        cost += weight_decay(cost, lstm_cf.tparams['U'], decay_c)
 
     f_cost = theano.function([x, mask, y], cost, name='f_cost')
-    grads = theano.grad(cost, wrt=list(tparams.values()))
+    grads = theano.grad(cost, wrt=list(lstm_cf.tparams.values()))
     f_grad = theano.function([x, mask, y], grads, name='f_grad')
 
     lr = T.scalar('lr')
-    f_grad_shared, f_update = optimizer(lr, tparams, grads, cost, x, mask, y)
+    f_grad_shared, f_update = optimizer(lr, lstm_cf.tparams, grads, cost, x, mask, y)
 
     print('Optimization')
 
@@ -251,24 +141,24 @@ def train_lstm(
                 if save_to and numpy.mod(uidx, save_freq) == 0:
                     print('Saving...')
                     if best_p is not None:
-                        params = best_p
+                        lstm_cf.params = best_p
                     else:
-                        params = unzip(tparams)
-                    numpy.savez(save_to, history_errs=history_errs, **params)
+                        lstm_cf.params = unzip(lstm_cf.tparams)
+                    numpy.savez(save_to, history_errs=history_errs, **lstm_cf.params)
                     pickle.dump(model_options, open('%s.pkl' % save_to, 'wb'),
                                 -1)
                     print('Done')
 
                 if numpy.mod(uidx, valid_freq) == 0:
                     use_noise.set_value(0.)
-                    train_err = pred_error(f_pred, train, kf)
-                    valid_err = pred_error(f_pred, valid, kf_valid)
-                    test_err = pred_error(f_pred, test, kf_test)
+                    train_err = lstm_cf.pred_error(train, kf)
+                    valid_err = lstm_cf.pred_error(valid, kf_valid)
+                    test_err = lstm_cf.pred_error(test, kf_test)
                     history_errs.append([valid_err, test_err])
 
                     if (best_p is None or valid_err <=
                             numpy.array(history_errs)[:, 0].min()):
-                        best_p = unzip(tparams)
+                        best_p = unzip(lstm_cf.tparams)
                         bad_counter = 0
 
                     print(('Train ', train_err, 'Valid ', valid_err,
@@ -292,15 +182,15 @@ def train_lstm(
 
     end_time = time.time()
     if best_p is not None:
-        zipp(best_p, tparams)
+        zipp(best_p, lstm_cf.tparams)
     else:
-        best_p = unzip(tparams)
+        best_p = unzip(lstm_cf.tparams)
 
     use_noise.set_value(0.)
     kf_train_sorted = get_minibatches_idx(len(train[0]), batch_size)
-    train_err = pred_error(f_pred, train, kf_train_sorted)
-    valid_err = pred_error(f_pred, valid, kf_valid)
-    test_err = pred_error(f_pred, test, kf_test)
+    train_err = lstm_cf.pred_error(train, kf_train_sorted)
+    valid_err = lstm_cf.pred_error(valid, kf_valid)
+    test_err = lstm_cf.pred_error(test, kf_test)
 
     print('Train ', train_err, 'Valid ', valid_err, 'Test ', test_err)
 
